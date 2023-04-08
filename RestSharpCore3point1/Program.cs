4e80ed6cb7b177b8649e1d821305609b56c89988
fernandodelgadosoftware <fernandodelgadosoftware@gmail.com>
2023-04-08 12:26:22 -0500

    initial commit

---
+++ RestSharpCore3point1/Program.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System;
+
+namespace RestSharpCore3point1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://arest.me");
+            //client.Timeout = -1;
+            //RestRequest request;
+            RestRequest request = new RestRequest("http://arest.me",Method.Get);
+            request.AddHeader("Authorization", "Basic YWVUcmFpbmluZzpaWlo=");
+            var body = @"";
+            request.AddParameter("text/plain", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
